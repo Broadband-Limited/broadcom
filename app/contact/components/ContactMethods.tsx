4a51af8a2c5e462de5ed6ec630e4018580cdad98
KigoJomo <kigojomo@gmail.com>
2025-06-23 23:15:31 +0300

    fix: standardize code formatting and improve button styles in ContactMethods component

--- app/contact/components/ContactMethods.tsx
+++ app/contact/components/ContactMethods.tsx
@@ -1,39 +1,41 @@
-'use client'
+'use client';
 
-import React, { useState } from 'react'
-import ContactForm from './ContactForm'
-import Offices from './Offices'
-import FAQ from './FAQ'
-import InfoHub from './InfoHub'
+import React, { useState } from 'react';
+import ContactForm from './ContactForm';
+import Offices from './Offices';
+import FAQ from './FAQ';
+import InfoHub from './InfoHub';
 
 const tabs = [
   { title: 'Form', component: ContactForm },
   { title: 'Offices', component: Offices },
   { title: 'FAQs', component: FAQ },
-]
+];
 
 const ContactMethods = () => {
-  const [activeTab, setActiveTab] = useState(0)
+  const [activeTab, setActiveTab] = useState(0);
 
-  const ActiveComponent = tabs[activeTab].component
+  const ActiveComponent = tabs[activeTab].component;
 
   return (
     <section className="py-32 grid grid-cols-1 md:grid-cols-3 gap-6">
       <div className="tabs w-full md:col-span-2 flex flex-col gap-4">
-        
-        <div className="tab-buttons w-full px-6 flex gap-6 md:gap-12 border-b border-black border-opacity-25">
+        <div className="tab-buttons w-full px-6 flex gap-2 border-b border-black border-opacity-25">
           {tabs.map((tab, index) => (
             <button
               key={index}
               onClick={() => setActiveTab(index)}
-              className={`border-b-2 px-4 ${activeTab === index ? 'text-light-blue opacity-100 border-light-blue' : 'text-foreground opacity-60 border-transparent hover:border-light-blue transition-all duration-300'}`}
-            >
+              className={`px-8 py-2 text-lg font-medium rounded-t-lg transition-all duration-300 
+                ${
+                  activeTab === index
+                    ? 'text-dark-blue opacity-100 border border-light-blue bg-dark-blue/30'
+                    : 'text-foreground opacity-75 border-b-2 border-transparent bg-dark-blue/10 hover:text-dark-blue hover:border-light-blue hover:bg-dark-blue/20 cursor-pointer'
+                }`}>
               {tab.title}
             </button>
           ))}
         </div>
 
-        
         <div className="w-full">
           <ActiveComponent />
         </div>
@@ -43,7 +45,7 @@ const ContactMethods = () => {
         <InfoHub />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ContactMethods
+export default ContactMethods;
